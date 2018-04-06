39beca2b6dab7a6435ac72a523416a8ba70bca6a
gataura <k612nn@gmail.com>
2018-04-06 22:30:14 +0300

    2.3 databases attempt 1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    objects = QuestionManager()
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='question_user')
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(blank=True, auto_now_add=True)
+    question = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='answer_question')
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='author_user')
---
+++ mysql.sh
@@ -0,0 +1,4 @@
++mysql -uroot -e "CREATE DATABASE qamysql;"
++mysql -uroot -e "CREATE USER 'django@localhost' IDENTIFIED BY 'salikh612';"
++mysql -uroot -e "GRANT ALL ON qamysql.* TO 'django@localhost';"
++mysql -uroot -e "FLUSH PRIVILEGES;"
